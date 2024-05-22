968b530850d6238507378aa1081796122e22c5a1
asifkarim <asif.karim100@gmail.com>
2024-05-22 07:08:02 +0530

    HY

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes,Route } from 'react-router-dom';
+import React from 'react'
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import MainHeader from './MainHeader';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Routes>
+        <Route path="/"  element={<MainHeader/>}>
+
+    <Route index element={<Home></Home>}/>
+    <Route path="about" element={<About></About>}></Route>
+    <Route path="contact" element={<Contact></Contact>}></Route>
+    </Route>
+   </Routes>
+   
+
+        
+      
       </header>
     </div>
   );
